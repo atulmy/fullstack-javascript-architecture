ee44edce6a6a17124cde1779f7c4cf6bc9e64473
atulmy <atul.12788@gmail.com>
2019-03-12 23:06:29 +0530

    Database connectivity fix

--- backend/api/src/setup/server/database.js
+++ backend/api/src/setup/server/database.js
@@ -5,8 +5,24 @@ import mongoose from 'mongoose'
 import { MONGO_URL } from '../config/env'
 
 // Connect database
-export default function () {
+export default async function () {
   console.info('SETUP - Connecting database..')
 
-  mongoose.connect(MONGO_URL, { useNewUrlParser: true, useCreateIndex: true })
+  await connectWithRetry()
+}
+
+// Handle connection error
+mongoose.connection.on('error', error => {
+  console.log(`ERROR - Connection failed: ${ error.message }`)
+
+  setTimeout(async () => {
+    console.log('SETUP - Connecting database.. retrying..')
+
+    await connectWithRetry()
+  }, 5000)
+})
+
+// Retry connection
+const connectWithRetry = async () => {
+  return await mongoose.connect(MONGO_URL, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
 }
