15fc31f8b35c61d1bdf4285c4e18c00b067d45dc
atulmy <atul.12788@gmail.com>
2020-03-08 14:50:49 +0800

    Updated dependencies

--- frontend/app/mobile/src/setup/config/env.js
+++ frontend/app/mobile/src/setup/config/env.js
@@ -2,6 +2,6 @@
 
 export const APP_ENV = 'development'
 
-export const API_URL = 'http://10.1.4.42:8000'
-export const WEB_URL = 'http://10.1.4.42:5000'
-export const LANDING_URL = 'http://10.1.4.42:3000'
+export const API_URL = 'http://192.168.1.1:8000'
+export const WEB_URL = 'http://192.168.1.1:5000'
+export const LANDING_URL = 'http://192.168.1.1:3000'
